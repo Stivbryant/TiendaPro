2ae294e2e729310b9556147c10b934ecd4c146a4
Bryan Barrera <bbarrer@est.ups.edu.ec>
2026-02-18 23:28:29 -0500

    Refactor Config class to use os.environ and update defaults

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
+
+    # Render suele dar DATABASE_URL en formato postgres://
+    uri = os.environ.get("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    STORE_NAME = os.getenv("STORE_NAME", "Tienda")
-    STORE_RUC = os.getenv("STORE_RUC", "")
-    STORE_ADDRESS = os.getenv("STORE_ADDRESS", "")
-    STORE_PHONE = os.getenv("STORE_PHONE", "")
-    TAX_RATE = float(os.getenv("TAX_RATE", "0.15"))
+    STORE_NAME = os.environ.get("STORE_NAME", "Progreso Tienda")
+    STORE_RUC = os.environ.get("STORE_RUC", "9999999999")
+    STORE_ADDRESS = os.environ.get("STORE_ADDRESS", "Quito, Ecuador")
+    STORE_PHONE = os.environ.get("STORE_PHONE", "0999999999")
+    TAX_RATE = float(os.environ.get("TAX_RATE", "0.12"))
